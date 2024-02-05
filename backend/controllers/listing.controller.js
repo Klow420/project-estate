6c360f0863fc62387dfe12fad9a4783857bead06
Klow420 <keiran-m420@hotmail.com>
2024-02-05 09:23:54 +0400

    create update listing api route

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -1,5 +1,6 @@
 import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
+import mongoose from "mongoose";
 
 export const createListing = async (req, res, next) =>{
     try {
@@ -13,7 +14,7 @@ export const createListing = async (req, res, next) =>{
 export const deleteListing = async (req, res, next) => {
     const listing = await Listing.findById(req.params.id);
     if(!listing) {
-        return next(errorHandler(404, 'Listing not found!'));
+        return next(error) ;
     }
     if(req.user.id !== listing.userRef) {
         return next(errorHandler(401, 'You can only delete your own listings!'));
@@ -21,6 +22,32 @@ export const deleteListing = async (req, res, next) => {
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json('Listing has been deleted!');
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const updateListing = async (req, res, next) => {
+    try {
+        if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+            throw errorHandler(404, 'Listing not found!');
+        }
+
+        const listing = await Listing.findById(req.params.id);
+        if (!listing) {
+            throw errorHandler(404, 'Listing not found!');
+        }
+        if (req.user.id !== listing.userRef.toString()) {
+            throw errorHandler(401, 'You can only update your own listing!');
+        }
+
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+
+        res.status(200).json(updatedListing);
     } catch (error) {
         next(error);
     }
